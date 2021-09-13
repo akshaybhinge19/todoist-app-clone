790f1757f05ee66174bfb47cbd96c4a0fcd19d63
Akshay <akshaybhinge19@gmail.com>
2021-09-13 13:04:08 +0530

    added with selector background functionality

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,9 +5,9 @@ const Sidebar = ({selectedTab, setselectedTab}) => {
     // console.log(selectedTab);
     return (
         <div className="sidebar">
-            <div onClick={()=> setselectedTab("Inbox")}><FaInbox className="icon"/>Inbox</div>
-            <div onClick={()=> setselectedTab("Today")}><FaRegCalendarAlt className="icon"/>Today</div>
-            <div onClick={()=> setselectedTab("Next_7")}><FaRegCalendar className="icon"/>Next 7 Days</div>
+            <div className={selectedTab==='Inbox' ? `active`:"" } onClick={()=> setselectedTab("Inbox")}><FaInbox className="icon"/>Inbox</div>
+            <div className={selectedTab==='Today' ? `active`:"" } onClick={()=> setselectedTab("Today")}><FaRegCalendarAlt className="icon"/>Today</div>
+            <div className={selectedTab==='Next_7' ? `active`:"" } onClick={()=> setselectedTab("Next_7")}><FaRegCalendar className="icon"/>Next 7 Days</div>
         </div>
     )
 }
